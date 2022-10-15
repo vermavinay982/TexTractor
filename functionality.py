bfb2196a06d684caaf59037ad627b947fd23cee7
vermavinay982 <vermavinay982@gmail.com>
2022-10-15 15:43:55 +0530

    Connected GUI with OCR

---
+++ functionality.py
@@ -0,0 +1,9 @@
+from extract_text import *
+
+def show():
+    print("Showing the text")
+
+
+def find_text(file, search):
+    print("Extracting the text from",file)
+    find_text_video(file, search)
